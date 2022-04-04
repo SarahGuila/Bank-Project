b080a8e57242ad4b296a6ba6ff6c52c9ed6ba902
Sarah Guila <sarahguila26@gmail.com>
2022-04-04 21:20:17 +0300

    Added data info actions

--- routes/data.js
+++ routes/data.js
@@ -1,2 +1,80 @@
+const { onlyLogged } = require('../middlewares/onlyLogged')
+const { Accounts } = require('../mongo/modelAccount')
+const { Logs } = require('../mongo/modelLoges')
+
 const router = require('express').Router()
-module.exports = router 
+
+
+/**
+ * @openapi
+ * /api/data/history:
+ *   get:
+ *     description: returns account's actions history.
+ *     tags: [Data]
+ *     responses:
+ *       200:
+ *         description: ok.
+ *       401:
+ *         description: please log in.
+ */
+router.get('/history', onlyLogged, async (req, res) => {
+    try {
+        const account = await Accounts.findById(req.session.account.id).populate({
+            path: 'actions.to',
+            select: 'username'
+        }).populate({
+            path: 'actions.from',
+            select: 'username'
+        })
+        res.send({ history: account.actions, balance: account.balance })
+    } catch (err) {
+        res.sendStatus(500)
+        console.log(err)
+    }
+})
+
+/**
+ * @openapi
+ * /api/data/accounts:
+ *   get:
+ *     description: returns accounts search results.
+ *     tags: [Data]
+ *     responses:
+ *       200:
+ *         description: ok.
+ *       401:
+ *         description: please log in.
+ */
+router.get('/accounts', onlyLogged, async (req, res) => {
+    try {
+        const accounts = await Accounts.find({}, { username: 1 })
+        res.send(accounts)
+    } catch (err) {
+        res.sendStatus(500)
+        console.log(err)
+    }
+})
+
+/**
+ * @openapi
+ * /api/data/logs:
+ *   get:
+ *     description: returns visit and actions log of all accounts.
+ *     tags: [Logs]
+ *     responses:
+ *       200:
+ *         description: ok.
+ *       401:
+ *         description: please log in.
+ */
+router.get('/logs', onlyLogged, async (req, res) => {
+    try {
+        const logs = await Logs.find({})
+        res.send(logs)
+    } catch (err) {
+        res.sendStatus(500)
+        console.log(err)
+    }
+})
+
+module.exports = router
